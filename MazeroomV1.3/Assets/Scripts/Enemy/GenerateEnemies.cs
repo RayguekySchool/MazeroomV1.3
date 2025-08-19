b9b63a6e958f70de7460085081f18a61a747f5ea
RayguekySchool <101849@selwyn.school.nz>
2025-08-19 13:32:54 +1200

    incomplete horde

--- MazeroomV1.3/Assets/Scripts/Enemy/GenerateEnemies.cs
+++ MazeroomV1.3/Assets/Scripts/Enemy/GenerateEnemies.cs
@@ -10,27 +10,70 @@ public class GenerateEnemies : MonoBehaviour
     public int maxEnemies;
     public Vector2 spawnXRange;
     public Vector2 spawnZRange;
+    public TextMeshProUGUI hordeText;
 
     private Transform spawnPoint;
+    private int currentHorde = 1;
+    private bool waitingForNextHorde = false;
 
     void Start()
     {
         spawnPoint = transform;
+        ShowHordeText();
         StartCoroutine(EnemyDrop());
+    }
+
+    void Update()
+    {
+        if (!waitingForNextHorde && GameObject.FindGameObjectsWithTag("Enemy").Length == 0 && enemyCount == maxEnemies)
+        {
+            waitingForNextHorde = true;
+            StartCoroutine(WaitAndStartNextHorde());
+        }
     }
 
     IEnumerator EnemyDrop()
     {
+        enemyCount = 0;
         while (enemyCount < maxEnemies)
         {
             float xPos = Random.Range(spawnXRange.x, spawnXRange.y);
             float zPos = Random.Range(spawnZRange.x, spawnZRange.y);
 
-            Vector3 spawnPosition = spawnPoint.position + new Vector3(xPos, 0, zPos);
+            // Y sempre 1
+            Vector3 spawnPosition = new Vector3(spawnPoint.position.x + xPos, 1f, spawnPoint.position.z + zPos);
 
             Instantiate(theEnemy, spawnPosition, Quaternion.identity);
             yield return new WaitForSeconds(0.1f);
             enemyCount++;
+        }
+    }
+
+    IEnumerator WaitAndStartNextHorde()
+    {
+        yield return new WaitForSeconds(5f);
+        currentHorde++;
+        ShowHordeText();
+        waitingForNextHorde = false;
+        StartCoroutine(EnemyDrop());
+    }
+
+    void ShowHordeText()
+    {
+        if (hordeText != null)
+        {
+            hordeText.text = $"Horde {currentHorde}";
+            hordeText.gameObject.SetActive(true);
+            StartCoroutine(HideHordeTextAfterSeconds(2f));
+        }
+    }
+
+    IEnumerator HideHordeTextAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        if (hordeText != null)
+        {
+            hordeText.gameObject.SetActive(false);
         }
     }
 
