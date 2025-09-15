8be7c7a7cc53fc1a5e7c3fd8c6ab7adfd5fe049b
RayguekySchool <101849@selwyn.school.nz>
2025-09-15 15:09:49 +1200

    good

--- MazeroomV1.3/Assets/Scripts/GameManager.cs
+++ MazeroomV1.3/Assets/Scripts/GameManager.cs
@@ -1,14 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+
     void Start()
     {
         
     }
-
+        
     void Update()
     {
 
