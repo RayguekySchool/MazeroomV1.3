246328603f953fc8048e833350391d722cb14300
RayguekySchool <101849@selwyn.school.nz>
2025-10-07 13:31:16 +1300

    New Scenes and finishing the Mani Menu

--- MazeroomV1.3/Assets/Scripts/Menu/MenuManager.cs
+++ MazeroomV1.3/Assets/Scripts/Menu/MenuManager.cs
@@ -1,63 +1,27 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu_Manager : MonoBehaviour
 {
-    public string DoomLike;
+    public string nameOfTheLevel1;
     public GameObject MainMenuPanel;
     public GameObject MenuPanelOptions;
-    public GameObject SelectLevelPanel;
-    public GameObject EventSystemMainMenu;
-    public GameObject EventSystemOptions;
-    public GameObject EventSystemMenuLevelSelect;
-    public Animator transitionAnimator;
-
-    public void OpenPlay()
-    {
-        MainMenuPanel.SetActive(false);
-        EventSystemMainMenu.SetActive(false);
-        SelectLevelPanel.SetActive(true);
-        EventSystemMenuLevelSelect.SetActive(true);
-    }
-
-    public void ClosePlay()
-    {
-        MainMenuPanel.SetActive(true);
-        EventSystemMainMenu.SetActive(true);
-        SelectLevelPanel.SetActive(false);
-        EventSystemMenuLevelSelect.SetActive(false);
-    }
 
     public void SelectLevel1()
     {
-        StartCoroutine(PlayTransitionAndLoad());
-    }
-
-    private IEnumerator PlayTransitionAndLoad()
-    {
-        if (transitionAnimator != null)
-        {
-            transitionAnimator.SetTrigger("Start");
-        }
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene(DoomLike);
+        SceneManager.LoadScene(nameOfTheLevel1);
     }
 
     public void OpenOptions()
     {
         MainMenuPanel.SetActive(false);
-        EventSystemMainMenu.SetActive(false);
         MenuPanelOptions.SetActive(true);
-        EventSystemOptions.SetActive(true);
     }
 
     public void CloseOptions()
     {
         MainMenuPanel.SetActive(true);
-        EventSystemMainMenu.SetActive(true);
         MenuPanelOptions.SetActive(false);
-        EventSystemOptions.SetActive(false);
     }
 
     public void Quit()
